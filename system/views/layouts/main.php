f822d22501c6a3a72182462125f64097c9d54ff0
Alkagar <alkagar@gmail.com>
2013-04-23 22:01:53 +0200

    configure main layout

--- system/views/layouts/main.php
+++ system/views/layouts/main.php
@@ -2,14 +2,15 @@
 <html>
     <head>
         <title></title>
-        <meta name="description" content="" />
-        <meta name="keywords" content="" />
+        <meta name="description" content="<?php echo Yii::t('site', 'head.description');?>" />
+        <meta name="keywords" content="<?php echo Yii::t('site', 'head.keywords');?>" />
         <meta name="author" content="Jakub Alkagar Mrowiec - jakub@mrowiec.org" />
         <meta charset="utf-8" />
         <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.6.0/build/cssreset/cssreset-min.css">
-        <link rel="stylesheet" type="text/css" href="*.css" />
-        <script type="text/javascript" src="scripts/"></script>
+        <link rel="stylesheet" type="text/css" href="default.css" />
+        <script type="text/javascript" src="scripts/default.js"></script>
     </head>
     <body>
+        <?php echo $content; ?>
     </body>
 </html>
