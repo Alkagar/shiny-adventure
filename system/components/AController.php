29b43713e4d0dced0c12fd0eb7b3c5bce3c69284
Alkagar <alkagar@gmail.com>
2013-04-23 21:42:15 +0200

    -- set default language to polish in AController

--- system/components/AController.php
+++ system/components/AController.php
@@ -2,4 +2,11 @@
     class AController extends CController
     {
         public $layout='//layouts/base';
+
+        public function init() 
+        {
+            parent::init();
+
+            Yii::app()->language = 'pl_pl';
+        }
     }
